ebd24db44566b8589938893566d6b0c82975daa8
Delta <delta@deltawolf.tk>
2021-08-27 16:51:38 -0700

    Switch metadata to line comments

--- DisableFocusCheck/DisableFocusCheck.meta.js
+++ DisableFocusCheck/DisableFocusCheck.meta.js
@@ -1,13 +1,13 @@
-/* ==UserScript==
-@name         DisableFocusChecking
-@author       Delta
-@description  Webpages should always think they are in focus when using this.
-@version      0.1
-@namespace    https://github.com/XDelta/UserScripts/
-@supportURL   https://github.com/XDelta/UserScripts/issues
-@updateURL    https://github.com/XDelta/UserScripts/DisableFocusCheck/DisableFocusCheck.meta.js
-@downloadURL  https://github.com/XDelta/UserScripts/DisableFocusCheck/DisableFocusCheck.user.js
-@grant        none
-@include      *
-@run-at document-start
-==/UserScript== */
+// ==UserScript==
+// @name         DisableFocusChecking
+// @author       Delta
+// @description  Webpages should always think they are in focus when using this.
+// @version      0.1.1
+// @namespace    https://github.com/XDelta/UserScripts/
+// @supportURL   https://github.com/XDelta/UserScripts/issues
+// @updateURL    https://github.com/XDelta/UserScripts/DisableFocusCheck/DisableFocusCheck.meta.js
+// @downloadURL  https://github.com/XDelta/UserScripts/DisableFocusCheck/DisableFocusCheck.user.js
+// @grant        none
+// @include      *
+// @run-at document-start
+// ==/UserScript==
